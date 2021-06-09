a904c132d37d18f351c5ac1cd0aa0efe8304680c
Navalhurpade <68598490+Navalhurpade@users.noreply.github.com>
2021-06-09 20:32:55 +0530

    Delete Client directory

--- Client/app/components/Lists/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ListItem } from './ListItem'
-export { default as ListItemSeperator } from './ListItemSeperator'
-export { default as RightSwipeAction } from './RightSwipeAction'
